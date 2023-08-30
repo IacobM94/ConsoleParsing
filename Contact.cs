d3a092a6586680466ce0f39c928997a70d13a1ba
Mihai Iacob <Mihai.Iacob@crimson.co.uk>
2023-08-30 12:03:25 +0100

    Add project files.

---
+++ Contact.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleParsing
+{
+    internal class Contact
+    {
+        private string Name;
+        private string DOB;
+        private string Email;
+        private string PhoneNumber;
+        private DateTime parsedDOB;
+        public int age;
+        private string FirstName;
+        private string LastName;
+
+        //constructor for rows which provide all the info we're looking for
+        public Contact(string name, string dOB, string email, string phoneNumber)
+        {
+            Name = name;
+            DOB = dOB;
+            Email = email;
+            PhoneNumber = phoneNumber;
+
+            //parse date of birth and calculate age
+            parsedDOB = DateTime.Parse(DOB);
+            int now = int.Parse(DateTime.Now.ToString("yyyyMMdd"));
+            int birthDate = int.Parse(parsedDOB.ToString("yyyyMMdd"));
+            age = (now - birthDate) / 10000;
+
+            //get First & Last Name from Name;
+            string[] firstAndLastName = Name.Split(' ');
+            FirstName = firstAndLastName[0];
+            LastName = firstAndLastName[1];
+
+            //adding white space to the mobile number
+            if (PhoneNumber != null && PhoneNumber.Length > 5)
+            {
+                PhoneNumber = PhoneNumber.Insert(5, " ");
+                //removing excess characters if phone number too long
+                if (PhoneNumber.Length > 12)
+                {
+                   PhoneNumber = PhoneNumber.Substring(0, 12);
+                }
+            }
+        }
+
+        //constructor which accounts for telephone number missing, thus setting it to null.
+        public Contact(string name, string dOB, string email):
+            this(name, dOB, email, null)
+        {
+        }
+
+        public void printDetails()
+        {
+            Console.WriteLine(FirstName.PadRight(15) + " " + LastName.PadRight(15) + " " + DOB.PadRight(15) + " " + age + "   " + Email.PadRight(30) + " " + PhoneNumber);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+using System.ComponentModel.Design;
+
+namespace ConsoleParsing
+{
+    internal class Program
+    {
+        //read .txt file & remove any blank or null results.
+        //Code will find the text file relative to the project directory
+        public static string projectFolder = Directory.GetCurrentDirectory();
+        public static string grandParentDirectory = Directory.GetParent(Directory.GetParent(projectFolder).FullName).FullName;
+        public static string filePath = Path.Combine(grandParentDirectory, @"docs\contacts.txt");
+        public static string[] contactDetails = File.ReadAllLines(filePath);
+
+        static void Main(string[] args)
+        {
+
+            //cleanup the data.
+            contactDetails = contactDetails.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+
+            //create an array of contacts
+            Contact[] Contacts = new Contact[contactDetails.Length];
+
+
+            //add data into each Contact instance
+            for (int i = 0; i < contactDetails.Length; i++)
+            {
+                char[] separators = new char[] { '\t' };
+                string[] parsedContact = contactDetails[i].Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (parsedContact.Length >= 4)
+                {
+                    Contacts[i] = new Contact(parsedContact[0], parsedContact[1], parsedContact[2], parsedContact[3]);
+                }
+                else
+                {
+                    Contacts[i] = new Contact(parsedContact[0], parsedContact[1], parsedContact[2]);
+                }
+            }
+
+            //sort the Contacts by age - this will sort in ascending order
+            Array.Sort(Contacts, new AgeComparer());
+            //reverse the order to show age in descending order
+            Array.Reverse(Contacts);
+            Console.WriteLine("First Name".PadRight(16) + "Last Name".PadRight(16) + "Date of Birth".PadRight(16) + "Age".PadRight(5) + "Email".PadRight(31) + "Phone Number");
+            Console.WriteLine("------------------------------------------------------------------------------------------------");
+            for (int i = 0; i < Contacts.Length; i++)
+            {
+                Contacts[i].printDetails();
+            }
+
+            Console.WriteLine("\nContacts Parsed!\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
